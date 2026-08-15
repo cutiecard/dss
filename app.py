efd9066b1fb81cd06910e1b525d1770a04d83b4a
cutiecard <cutiecard911@gmail.com>
2026-08-15 01:50:24 -0300

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ load_dotenv()
 if __name__ == "__main__":
     uvicorn.run(
         "server.api:app",
-        host=os.getenv("HOST", "127.0.0.1"),
+        host=os.getenv("HOST", "0.0.0.0"),
         port=int(os.getenv("PORT", "8000")),
         reload=False,
     )
